f336a64943db6ac3fef470197ebbacc83250d482
a-zz <antonio@pareidolia.es>
2018-12-07 12:09:20 +0100

    Data model for jrss placed into DB updater.

--- source/java/io/github/azz/sql/da/DbUpdaterHSQLDB.java
+++ source/java/io/github/azz/sql/da/DbUpdaterHSQLDB.java
@@ -66,6 +66,38 @@ public class DbUpdaterHSQLDB implements DbUpdaterDaInterface, HSQLDBInterface {
 		}
 	}
 		
+	public void updateToVersion1(Boolean unattended) throws SQLException {
+		
+		SqlTransaction t = null;
+
+		try {
+			t = new SqlTransaction("db update to version 1", true);
+			t.statement("create table STACKS " +
+					"(ID integer identity primary key, " +
+					"STACKID varchar(1024) not null)");
+			t.statement("create unique index IDX_STACK on STACKS (STACKID)");
+			t.statement("create table STACK_VALUES " +
+					"(ID integer identity primary key, " +
+					"STACK_ID integer, " +
+					"TS timestamp default current_timestamp not null, " +
+					"DATA clob)");
+			t.statement("alter table STACK_VALUES add foreign key (STACK_ID) references STACKS (ID)");
+			saveVersionInfo(1, "Data model for jrss", t);
+		}
+		catch(SQLException e) {
+			throw e;
+		}
+		finally {
+			t.close();
+		}
+		/*
+		 * ;
+;
+;
+;
+		 */
+	}
+	
 	public void saveVersionInfo(int version, String description, SqlTransaction t) throws SQLException {
 	
 		t.statement("insert into DBVERSION (VERSION, DESCRIPTION) "
